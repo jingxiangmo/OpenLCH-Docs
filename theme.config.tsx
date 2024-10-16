04eadabcd0f7191f77e48490de7d7cc9c81ee5b4
JX <jingxiangmo@gmail.com>
2024-10-16 14:26:09 -0700

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>OpenLCH</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Zeroth-Robotics/OpenLCH',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.com/invite/kscale',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/jingxiangmo/openlch-docs/tree/main',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'OpenLCH, a K-Scale Labs project.',
   },
 }
 
