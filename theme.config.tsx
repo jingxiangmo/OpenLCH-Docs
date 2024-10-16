2b2a1910435d63a62f00f75e0087ac3110a20640
JX <jingxiangmo@gmail.com>
2024-10-16 15:08:03 -0700

    update meta

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router';
+import { useConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>OpenLCH</span>,
+  logo: <span>OpenLCH Docs</span>,
   project: {
     link: 'https://github.com/Zeroth-Robotics/OpenLCH',
   },
@@ -13,6 +15,22 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'OpenLCH, a K-Scale Labs project.',
   },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url =
+      'https://docs.kscale.dev' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
+    return (
+      <>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={'OpenLCH Docs'} />
+        <meta property="og:description" content={'OpenLCH is an open source humanoid building guide'} />
+      </>
+    );
+  }
 }
 
 export default config
