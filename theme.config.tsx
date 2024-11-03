b9303ec482c2a639d8adc82a34105fce13b3ce20
JX <jingxiangmo@gmail.com>
2024-11-03 00:58:05 -0700

    update meta

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,30 +4,30 @@ import { useRouter } from 'next/router';
 import { useConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>OpenLCH Docs</span>,
+  logo: <span>Zeroth Robotics Docs</span>,
   project: {
     link: 'https://github.com/Zeroth-Robotics/OpenLCH',
   },
   chat: {
-    link: 'https://discord.com/invite/kscale',
+    link: 'https://discord.gg/aHTY4ur4',
   },
   docsRepositoryBase: 'https://github.com/jingxiangmo/openlch-docs/tree/main',
   footer: {
-    text: 'OpenLCH, a Zeroth Robotics project.',
+    text: 'Zeroth Robotics, made with ❤️ by JX, Denys, Kelsey, and friends.',
   },
   head: () => {
     const { asPath, defaultLocale, locale } = useRouter();
     const { frontMatter } = useConfig();
     const url =
-      'https://docs.kscale.dev' +
+      'https://docs.zeroth.bot' +
       (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
 
     return (
       <>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:url" content={url} />
-        <meta property="og:title" content={'OpenLCH Docs'} />
-        <meta property="og:description" content={'OpenLCH is an open source humanoid building guide'} />
+        <meta property="og:title" content={'Zeroth Robotics Docs'} />
+        <meta property="og:description" content={'powerful, affordable, and open-source robotics platform for builders'} />
       </>
     );
   }
