616cfe0e87c55e1b49e062351ba0bf51744d93c7
JX <jingxiangmo@gmail.com>
2024-11-10 22:30:06 -0800

    fix datalayer and links

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,23 @@
-// pages/_app.js
 import { GoogleAnalytics } from 'nextjs-google-analytics';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
       <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </>
